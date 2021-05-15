aea796152e65a33a3d52a422cf77f0b84a1b71a6
Sarvadnya mahajan <80883695+SarvadnyaMahajan@users.noreply.github.com>
2021-05-15 16:57:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -99,4 +99,64 @@ function new_image(get_image) {
                         console.log("u")
                         }
 
+                        if (keyPressed=='38') {
+                        up()
+                        console.log("up")
+                        }
+
+                        if (keyPressed=='40') {
+                            down()
+                            console.log("down")
+                            }
+
+                            if (keyPressed=='37') {
+                                left()
+                                console.log("left")
+                                }
+
+                                if (keyPressed=='39') {
+                                    right()
+                                    console.log("right")
+                                } 
+                                
+
     }
+    function up() {
+        if (player_y>=0) {
+        player_y=player_y-block_image_height
+        console.log("block image height="+block_image_height)
+        console.log("when up arrow key is pressed,x="+player_x+"y="+player_y)
+        canvas.remove(player_object);
+        player_update()
+        }
+    }
+
+    function down() {
+        if (player_y<=500) {
+        player_y=player_y+block_image_height
+        console.log("block image height="+block_image_height)
+        console.log("when down arrow key is pressed,x="+player_x+"y="+player_y)
+        canvas.remove(player_object);
+        player_update()
+        }
+    }
+    
+    function left() {
+        if(player_x>=0) {
+            player_x=player_x-block_image_width
+            console.log("block image width="+block_image_width)
+            console.log("when left arrow key is pressed,x="+player_x+"y="+player_y)
+            canvas.remove(player_object);
+            player_update()
+        }
+    }
+
+    function right() {
+        if (player_x<=850) {
+            player_x=player_x+block_image_width
+            console.log("block image width="+block_image_width)
+            console.log("when right arrow key is pressed,x="+player_x+"y="+player_y)
+            canvas.remove(player_object);
+            player_update()
+        }
+    } 
